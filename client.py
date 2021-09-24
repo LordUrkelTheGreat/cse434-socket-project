090872868984beafc66f5c0c2d277d5d18c37c36
LordUrkelTheGreat <72357566+LordUrkelTheGreat@users.noreply.github.com>
2021-09-24 12:51:26 -0700

    2nd Update
    Added all of the functions needed for this project. Currently working on the register function.

--- client.py
+++ client.py
@@ -6,16 +6,38 @@ serverName = '127.0.0.1'
 serverPort = 11050
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientSocket.close()
+
+# message = raw_input('Input lowercase sentence:')
+# clientSocket.sendto(message.encode(), (serverName, serverPort))
+# modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+# print(modifiedMessage.decode())
+# clientSocket.close()
 
 
 # register <user-name> <IPv4-address> <port>
 def client_register():
-    print("qwert")
+    # prints register and then lets user input 3 strings separated by spaces
+    userName, ipAddr, portNum = input("register ").rsplit(None, 2)
+
+    if userName.isalpha() and len(userName) <= 15:
+        # the 1 lets server know this is a register command
+        clientSocket.sendto("1".encode(), (serverName, serverPort))
+
+        # send username, ip address, and port number to server
+        clientSocket.sendto(userName.encode(), (serverName, serverPort))
+        clientSocket.sendto(ipAddr.encode(), (serverName, serverPort))
+        clientSocket.sendto(portNum.encode(), (serverName, serverPort))
+
+        # command message returned and printed
+        commandMessage, serverAddress = clientSocket.recvfrom(2048)
+        print(commandMessage.decode())
+        exit()
+    else:
+        # when the conditions of the above if statement isnt met
+        print("Invalid input. Try again.")
+        exit()
 
 
+# input1 = list(map(str, input().split()))
+# if input1[0] in 'register':
 client_register()
--- server.py
+++ server.py
@@ -5,7 +5,87 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("RUNNING")
 
+
+def server_register():
+    # receive username, ip address, and port number from client
+    regName, clientAddress = serverSocket.recvfrom(2048)
+    regIP, clientAddress = serverSocket.recvfrom(2048)
+    regPort, clientAddress = serverSocket.recvfrom(2048)
+
+    # decode username, ip address, and port number
+    decodeName = regName.decode()
+    decodeIP = regIP.decode()
+    decodePort = regPort.decode()
+
+    success = "SUCCESS"
+
+    # return success message to client
+    serverSocket.sendto(success.encode(), clientAddress)
+
+
+def server_setupDHT():
+    print(2)
+
+
+def server_completeDHT():
+    print(3)
+
+
+def server_queryDHT():
+    print(4)
+
+
+def server_leaveDHT():
+    print(5)
+
+
+def server_rebuiltDHT():
+    print(6)
+
+
+def server_deregister():
+    print(7)
+
+
+def server_joinDHT():
+    print(8)
+
+
+def server_teardownDHT():
+    print(9)
+
+
+def server_teardownComplete():
+    print(10)
+
+
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    # message, clientAddress = serverSocket.recvfrom(2048)
+    # modifiedMessage = message.decode().upper()
+    # serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+    # receives encoded command from client and decodes it
+    encodedCommand, clientAddress = serverSocket.recvfrom(2048)
+    command = encodedCommand.decode()
+
+    # if the command is from 1-10, it will execute one of the many functions
+    if command == "1":
+        server_register()
+    if command == "2":
+        server_setupDHT()
+    if command == "3":
+        server_completeDHT()
+    if command == "4":
+        server_queryDHT()
+    if command == "5":
+        server_leaveDHT()
+    if command == "6":
+        server_rebuiltDHT()
+    if command == "7":
+        server_deregister()
+    if command == "8":
+        server_joinDHT()
+    if command == "9":
+        server_teardownDHT()
+    if command == "10":
+        server_teardownComplete()
